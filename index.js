ad594bd7d47c9a408647ebf568c57719e750ffd9
tobiasz121 <78846581+tobiasz121@users.noreply.github.com>
2022-04-08 13:15:51 +0200

    commit

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const button = document.querySelector('button')
+// const button = document.querySelector('button')
 
-button.addEventListener('click', event => {
-    fetch('./data.json')
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            console.log(data)
-        })
+// button.addEventListener('click', event => {
+//     fetch('./data.json')
+//         .then(response => {
+//             return response.json()
+//         })
+//         .then(data => {
+//             console.log(data)
+//         })
     
+// })
+
+
+const buttons = [... document.querySelectorAll('.header-interval > button')]
+buttons.forEach(button => {
+    console.log(button)
 })
+
+buttons.forEach(button => {
+    // console.log(button.textContent)    
+    button.addEventListener('click', event => {
+        buttons.forEach(btn => {
+            btn.classList.remove('active')
+        })        
+        event.currentTarget.classList.add('active')
+        fetch('./data.json')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    })
+
+  
+})
+
+
+
